a589092cf334a0e3ac314625d13970f1edd7ef66
NovusEdge <novusedge0@gmail.com>
2020-12-30 09:11:37 +0530

    Added some functions for vectors

--- Geometry/TwoDimension/vectors.go
+++ Geometry/TwoDimension/vectors.go
@@ -72,6 +72,22 @@ func (cv *CmpVector) UnitVector() CmpVector {
 	}
 }
 
-func (v *Vector) DotProduct() Vector {
+//DotProduct reports the dot product between 2 vectors
+func (v1 *Vector) DotProduct(v2 *CmpVector) float64 {
+	x_cmp1, y_cmp1 := v1.Point_2.X-v1.Point_1.X, v1.Point_2.Y-v1.Point_1.Y
+	x_cmp2, y_cmp2 := v2.Point_2.X-v2.Point_1.X, v2.Point_2.Y-v2.Point_1.Y
+	return x_cmp1*x_cmp2 + y_cmp1*y_cmp2
+}
 
+func (cv1 *CmpVector) DotProduct(cv2 *CmpVector) float64 {
+	return cv1.X_Component*cv2.X_Component + cv1.Y_Component*cv2.Y_Component
+}
+
+//Angle reports the angle between 2 vector
+func (v1 *Vector) Angle(v2 *Vector) float64 {
+	return math.Acos(v1.DotProduct(v2) / (v1.Magnitude() * v2.Magnitude()))
+}
+
+func (cv1 *CmpVector) Angle(cv2 *CmpVector) float64 {
+	return math.Acos(cv1.DotProduct(cv2) / (cv1.Magnitude() * cv2.Magnitude()))
 }
